a7ce342e9c821353b3d19835db7b2917c8c826e8
juancubobravo <juancubo@uma.es>
2022-04-28 13:52:26 +0200

    Arreglar R5

--- proyecto-ejb/src/main/java/CuentaEJB.java
+++ proyecto-ejb/src/main/java/CuentaEJB.java
@@ -1,6 +1,7 @@
 import exceptions.*;
 import uma.wow.proyecto.*;
 
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -10,19 +11,22 @@ import java.util.List;
 @Stateless
 public class CuentaEJB implements GestionCuenta {
 
+	@EJB
+	AccesoEJB acceso;
     @PersistenceContext(unitName="WOWEJB")
     private EntityManager em;
 
     // R5
     //@Override
-    public void creaCuenta(CuentaFintech cuentaNueva, Cliente c) throws CuentaEncontrada, ClienteNoEncontrado {
-        CuentaFintech cuenta = em.find(CuentaFintech.class, cuentaNueva.getIban());
+    public void creaCuenta(PooledAccount cuentaNueva, Empresa c, Usuario usuario) throws CuentaEncontrada, ClienteNoEncontrado, UsuarioNoEncontrado, ContraseniaInvalida, NoAdministradorException {
+    	acceso.loginAdministrador(usuario);
+        PooledAccount cuenta = em.find(PooledAccount.class, cuentaNueva.getIban());
 
         if(cuenta != null){
             throw new CuentaEncontrada();
         }
 
-        Cliente cliente = em.find(Cliente.class, c.getId());
+        Empresa cliente = em.find(Empresa.class, c.getId());
 
         if(cliente == null){
             throw new ClienteNoEncontrado();
@@ -33,6 +37,74 @@ public class CuentaEJB implements GestionCuenta {
             em.persist(cuentaNueva);
         }
     }
+    
+    // R5
+    //@Override
+    public void creaCuenta(Segregada cuentaNueva, Empresa c, Usuario usuario) throws CuentaEncontrada, ClienteNoEncontrado, UsuarioNoEncontrado, ContraseniaInvalida, NoAdministradorException {
+    	acceso.loginAdministrador(usuario);
+        Segregada cuenta = em.find(Segregada.class, cuentaNueva.getIban());
+
+        if(cuenta != null){
+            throw new CuentaEncontrada();
+        }
+
+        Empresa cliente = em.find(Empresa.class, c.getId());
+
+        if(cliente == null){
+            throw new ClienteNoEncontrado();
+        } else {
+            List<CuentaFintech> listCuenta = cliente.getCuentas();
+            listCuenta.add(cuentaNueva);
+            cliente.setCuentas(listCuenta);
+            em.persist(cuentaNueva);
+        }
+    }
+    
+    //R5
+    @Override
+    public void creaCuenta(PooledAccount cuentaNueva, Individual c, Usuario usuario) throws CuentaEncontrada, ClienteNoEncontrado, UsuarioNoEncontrado, ContraseniaInvalida, NoAdministradorException {
+    	acceso.loginAdministrador(usuario);
+        PooledAccount cuenta = em.find(PooledAccount.class, cuentaNueva.getIban());
+
+        if(cuenta != null){
+            throw new CuentaEncontrada();
+        }
+
+        Individual cliente = em.find(Individual.class, c.getId());
+
+        if(cliente == null){
+            throw new ClienteNoEncontrado();
+        } else {
+            List<CuentaFintech> listCuenta = cliente.getCuentas();
+            listCuenta.add(cuentaNueva);
+            cliente.setCuentas(listCuenta);
+            em.persist(cuentaNueva);
+        }
+    }
+    
+    // R5
+    //@Override
+    public void creaCuenta(Segregada cuentaNueva, Individual c, Usuario usuario) throws CuentaEncontrada, ClienteNoEncontrado, UsuarioNoEncontrado, ContraseniaInvalida, NoAdministradorException {
+    	acceso.loginAdministrador(usuario);
+        Segregada cuenta = em.find(Segregada.class, cuentaNueva.getIban());
+
+        if(cuenta != null){
+            throw new CuentaEncontrada();
+        }
+
+        Individual cliente = em.find(Individual.class, c.getId());
+
+        if(cliente == null){
+            throw new ClienteNoEncontrado();
+        } else {
+            List<CuentaFintech> listCuenta = cliente.getCuentas();
+            listCuenta.add(cuentaNueva);
+            cliente.setCuentas(listCuenta);
+            em.persist(cuentaNueva);
+        }
+    }
+    
+    
 
     // R9
     // TODO -- Revisar (¿a qué saldo se refiere el requisito?)
--- proyecto-ejb/src/main/java/GestionCuenta.java
+++ proyecto-ejb/src/main/java/GestionCuenta.java
@@ -4,7 +4,10 @@ import uma.wow.proyecto.*;
 
 public interface GestionCuenta {
 	
-    public void creaCuenta(CuentaFintech cuentaNueva, Cliente c) throws CuentaEncontrada, ClienteNoEncontrado;
+	public void creaCuenta(PooledAccount cuentaNueva, Individual c, Usuario usuario) throws CuentaEncontrada, ClienteNoEncontrado, UsuarioNoEncontrado, ContraseniaInvalida, NoAdministradorException;
+	public void creaCuenta(PooledAccount cuentaNueva, Empresa c, Usuario usuario) throws CuentaEncontrada, ClienteNoEncontrado, UsuarioNoEncontrado, ContraseniaInvalida, NoAdministradorException;
+	public void creaCuenta(Segregada cuentaNueva, Individual c, Usuario usuario) throws CuentaEncontrada, ClienteNoEncontrado, UsuarioNoEncontrado, ContraseniaInvalida, NoAdministradorException;
+	public void creaCuenta(Segregada cuentaNueva, Empresa c, Usuario usuario) throws CuentaEncontrada, ClienteNoEncontrado, UsuarioNoEncontrado, ContraseniaInvalida, NoAdministradorException;
     public void cierraCuenta(String IBAN) throws SaldoException, CuentaNoEncontrada;
 
 }
