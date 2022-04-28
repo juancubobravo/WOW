c4bddc1b366e95260052805137b59ac02fd31753
Josemi016 <jmiguel16@hotmail.es>
2022-04-28 12:43:32 +0200

    ClienteEJB

--- proyecto-ejb/src/main/java/ClienteEJB.java
+++ proyecto-ejb/src/main/java/ClienteEJB.java
@@ -1,24 +1,47 @@
 import java.util.List;
 
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import exceptions.ClienteNoEncontrado;
+import exceptions.ContraseniaInvalida;
 import exceptions.CuentasActivas;
 import exceptions.DatosException;
+import exceptions.NoAdministradorException;
 import exceptions.UsuarioException;
+import exceptions.UsuarioNoEncontrado;
 import uma.wow.proyecto.*;
 
+@Stateless
 public class ClienteEJB implements GestionCliente{
 
+	@EJB
+	private AccesoEJB acceso;
     @PersistenceContext(unitName="WOWEJB")
     private EntityManager em;
 
     //R2
     @Override
-    public void altaCliente(Cliente cliente, Usuario usuario) throws UsuarioException{
+    public void altaCliente(Empresa cliente, Usuario usuario) throws UsuarioException, UsuarioNoEncontrado, ContraseniaInvalida, NoAdministradorException{
+    	
+    	acceso.loginAdministrador(usuario);
 
-        Cliente busc = em.find(Cliente.class, cliente.getId());
+        Empresa busc = em.find(Empresa.class, cliente.getId());
+
+        if(busc != null){
+            throw new UsuarioException();
+        }
+
+        em.persist(cliente);
+    }
+    
+    public void altaCliente(Individual cliente, Usuario usuario) throws UsuarioException, UsuarioNoEncontrado, ContraseniaInvalida, NoAdministradorException{
+    	
+    	acceso.loginAdministrador(usuario);
+
+        Individual busc = em.find(Individual.class, cliente.getId());
 
         if(busc != null){
             throw new UsuarioException();
